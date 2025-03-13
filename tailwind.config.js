1c5cef1b13ce9ab09497a2aedc1a7053d9424a21
cdenzelcoleman <camdcole@gmail.com>
2025-03-13 11:19:13 -0500

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        // Add pixel-art style extensions here
+      },
+    },
+    plugins: [],
+  }
