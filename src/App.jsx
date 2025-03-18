ea87aea669080a6369d1c3d70f1f71a18250d7a3
cdenzelcoleman <camdcole@gmail.com>
2025-03-18 15:51:56 -0500

    Add tailwind import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Skills from './pages/Skills'
-import Contact from './pages/Contact'
-import Bonus from './pages/Bonus'
-import './App.css'
+import React from 'react';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+import Bonus from './pages/Bonus';
 
 function App() {
   return (
     <div className="overflow-x-hidden">
-      <Navbar />
       <About />
       <Projects />
       <Skills />
